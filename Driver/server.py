adac5207abe26d8d8c7214b862bb327216e0f169
Hidde <hidde8erberg@gmail.com>
2018-09-21 15:59:42 +0200

    New terrain

--- Driver/server.py
+++ Driver/server.py
@@ -1,11 +1,17 @@
 import socket
 import numpy as np
 
+ip = 'localhost'
+port_receive = 11111
+port_send = 22222
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('localhost', 11111))
+sock.bind((ip, port_receive))
 
 while True:
     data, addr = sock.recvfrom(1024)
     distances = np.fromstring(data, np.float32)
-
     print(distances)
+
+    sock.sendto(1, (ip, port_send))
+    print("send successful")
