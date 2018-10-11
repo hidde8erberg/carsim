38a27cccd6d5e30b6dcfd14865df568566b27043
Hidde <hidde8erberg@gmail.com>
2018-10-11 22:02:21 +0200

    udp still not working

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -1,42 +1,32 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
 
-public class Server : MonoBehaviour {
-
-	private byte[] _data = new byte[1024];
+public class Server : MonoBehaviour
+{
+	private int recv;
+	private byte[] _data;
 	private IPEndPoint _ipep;
-	private UdpClient _sock;
+	private Socket _socket;
 	private IPEndPoint _sender;
+	private EndPoint _remote;
 	
 	void Start () {
-		try
-		{
-			_ipep = new IPEndPoint(IPAddress.Any, 22222);
-			_sock = new UdpClient(_ipep);
-
-			_sender = new IPEndPoint(IPAddress.Any, 0);
-		}
-		catch (Exception e)
-		{
-			Console.WriteLine(e);
-			throw;
-		}
+		_ipep = new IPEndPoint(IPAddress.Any, 6969);
+		_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		
+		_socket.Bind(_ipep);
+		Debug.Log("Waiting for a client...");
+		
+		_sender = new IPEndPoint(IPAddress.Any, 0);
+		_remote = (EndPoint) (_sender);
 	}
 
 	void FixedUpdate () {
-		try
-		{
-			_data = _sock.Receive(ref _sender);
-		
-			Debug.Log(_data);
-		}
-		catch (Exception e)
-		{
-			Console.WriteLine(e);
-			throw;
-		}
+		_data = new byte[1024];
+		recv = _socket.ReceiveFrom(_data, ref _remote);
+	
+		Debug.Log(Encoding.ASCII.GetString(_data, 0, recv));
 	}
 }
--- Driver/client.py
+++ Driver/client.py
@@ -1,18 +1,17 @@
 import socket
 # import numpy as np
-import sys
 
 
 def client():
     ip = 'localhost'
-    port = 22222
+    port = 6969
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((ip, port))
 
     while True:
-        data = [1, 1, 1]
-        sock.sendto(bytes(data), (ip, port))
+        data = "hello"
+        sock.sendto(bytes(data, "utf-8"), (ip, port))
 
 
 if __name__ == '__main__':
