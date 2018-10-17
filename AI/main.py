3bbe5609f9cda6fd265c748d74db6278cd7b4068
Hidde <hidde8erberg@gmail.com>
2018-10-17 16:21:32 +0200

    basic control

--- AI/main.py
+++ AI/main.py
@@ -2,3 +2,10 @@
 import numpy as np
 
 from connect import Connect
+
+if __name__ == '__main__':
+    conn = Connect()
+    while True:
+        a, b, c = conn.server()
+        if b > 65:
+            conn.client(1)
