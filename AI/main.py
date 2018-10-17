28414753bfe3045428f1defa83fb7ec9bd109921
Hidde <hidde8erberg@gmail.com>
2018-10-17 16:38:52 +0200

    Update AI/main.py

--- AI/main.py
+++ AI/main.py
@@ -2,10 +2,14 @@
 import numpy as np
 
 from connect import Connect
+import utils
 
 if __name__ == '__main__':
     conn = Connect()
     while True:
-        a, b, c = conn.server()
+        a, b, c = utils.sort_server(conn.server())
+        print(a)
+        print(b)
+        print(c)
         if b > 65:
             conn.client(1)
